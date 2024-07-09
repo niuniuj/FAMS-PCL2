7811ad462dbf926c909033b0afe6e96a0f00d6f3
牛牛君 <1007503677@qq.com>
2024-07-09 15:53:29 +0800

    加密密码

--- login.php
+++ login.php
@@ -2,9 +2,10 @@
     session_start();
     $username = $_POST['username'];
     $password = $_POST['password'];
+    $password = hash('sha256',$password);
     
     // 检查密码是否正确
-    if ($password === "FAMSserver114514" and $username === "admin") {
+    if ($password === "067b126a50ae65dffda8022d77cf161bd4b898937ea050e58a3ef884bd59e98b" and $username === "admin") {
         $_SESSION['username'] = $username; // 将用户名存储在会话中
 
         // 设置localStorage的loggedIn状态
